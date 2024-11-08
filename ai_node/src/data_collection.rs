1ad40bce576f857417584b780325d744c92c2f63
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-08 21:32:03 +0800

    Update data_collection.rs

--- ai_node/src/data_collection.rs
+++ ai_node/src/data_collection.rs
@@ -1,1 +1,28 @@
+use web3::futures::StreamExt;
+use web3::types::FilterBuilder;
+
+pub async fn collect_dapp_interaction_data() {
+    let transport = web3::transports::Http::new("https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID").unwrap();
+    let web3 = web3::Web3::new(transport);
+
+    let filter = FilterBuilder::default().build();
+    let logs_stream = web3.eth_subscribe().subscribe_logs(filter).await.unwrap();
+
+    logs_stream.for_each(|log| async {
+        match log {
+            Ok(log) => println!("New log: {:?}", log),
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }).await;
+}
+
+pub async fn collect_transaction_data() {
+    let transport = web3::transports::Http::new("https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID").unwrap();
+    let web3 = web3::Web3::new(transport);
+
+    let block_number = web3.eth().block_number().await.unwrap();
+    println!("Current block number: {:?}", block_number);
+
+    // Fetch and process transaction data
+}
 
